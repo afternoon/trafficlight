66a55f62260e469ad36b0f25c7f650ad1ce33ff7
Ben Godfrey <bpgodfrey@gmail.com>
2014-05-10 18:31:47 +0100

    Close serialport after control signals sent

--- bin/trafficlight-update.js
+++ bin/trafficlight-update.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-/* global require */
+/* global require, module */
+
+var portName = '/dev/cu.usbserial-A602U1ZL',
+    jenkinsUrl = 'http://jenkins.test.wonga.com';
 
 var SerialPort = require('serialport').SerialPort,
     Jenkins = require('jenkins'),
     Relay = require('../lib/relay');
     TrafficLight = require('../lib/trafficlight');
 
-var portName = '/dev/cu.Bluetooth-Incoming-Port',
-    // '/dev/cu.usbserial-A602U1ZL',
-    serialPort = new SerialPort(portName, {baudrate: 9600}),
-    jenkinsUrl = 'http://jenkins.test.wonga.com',
-    jenkins = Jenkins(jenkinsUrl),
+var serialPort = new SerialPort(portName, {baudrate: 9600}, false),
     relay = new Relay(serialPort),
+    jenkins = Jenkins(jenkinsUrl),
     tl = new TrafficLight(relay, jenkins);
 
 tl.update();
